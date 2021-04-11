5c79b224038a67e559cbaf07b8c45521146260fd
Zach Nicoll <zach.nicoll55@gmail.com>
2021-04-11 15:13:57 +1000

    feat: completed doc strings for all User methods

--- shared/src/models/User.java
+++ shared/src/models/User.java
@@ -23,7 +23,7 @@ public class User {
     private AccountType accountType;
 
     /**
-     * Organisation Unit the User belongs to.
+     * Organisational Unit the User belongs to.
      */
     private OrganisationalUnit organisationalUnit;
 
@@ -34,10 +34,11 @@ public class User {
     private String authenticationToken;
 
     /**
-     *
-     * @param username
-     * @param accountType
-     * @param organisationalUnit
+     * Construct a new User with given information, most likely provided by the API.
+     * @param userId Unique identifier for User
+     * @param username Displayed name of User
+     * @param accountType Representation of privileges given to User
+     * @param organisationalUnit Organisational Unit the User belongs to
      */
     User (String userId, String username, AccountType accountType, OrganisationalUnit organisationalUnit) {
         this.userId = userId;
@@ -47,9 +48,10 @@ public class User {
     }
 
     /**
-     *
-     * @param password
-     * @throws AuthenticationException
+     * Hash the given password and make a request to the /login API endpoint - if
+     * successful, the JWT token provided by the server will be stored in the User.
+     * @param password String will be hashed before being sent to server
+     * @throws AuthenticationException if login is unsuccessful
      */
     public void Login(String password) throws AuthenticationException {
         /**
@@ -68,29 +70,38 @@ public class User {
     }
 
     /**
-     *
+     * Remove the User's authentication token, rendering them 'unauthenticated'.
      */
     public void Logout() {
+        authenticationToken = null;
         return;
     }
 
     /**
-     *
-     * @return
+     * Determine whether or not a User has an authentication token that has not expired.
+     * @return true if authentication token is present and has not expired, false otherwise
      */
     public Boolean IsAuthenticated() {
-        return true;
+        if (authenticationToken != null) {
+            /**
+             * Check if authentication token has not expired
+             */
+
+            return true;
+        }
+        return false;
     }
 
     /**
-     *
-     * @param newAccountType
-     * @param newOrganisationalUnit
-     * @throws ApiException
+     * Update a User's attribute via the /user endpoint. If successful, the User object
+     * will also be updated.
+     * @param newAccountType updated AccountType
+     * @param newOrganisationalUnit updated OrganisationalUnit
+     * @throws ApiException if an error occurs while making the API request
      */
     public void updateUser(AccountType newAccountType, OrganisationalUnit newOrganisationalUnit) throws ApiException {
         /**
-         * Make API request here to update User, this API request throws ApiException
+         * Make PUT API request here to update User, this API request throws ApiException
          */
 
         accountType = newAccountType;
@@ -98,40 +109,47 @@ public class User {
     }
 
     /**
-     *
-     * @throws ApiException
+     * Change a User's password via the /user/[userId]/change-password endpoint
+     * @throws ApiException if an error occurs while making the API request
      */
-    public void deleteUser() throws ApiException {
+    public void changePassword(String password) throws ApiException {
+        /**
+         * Hash the password
+         */
+
+        /**
+         * Make POST request to the API to update password
+         */
         return;
     }
 
     /**
-     *
-     * @return
+     * Get the AccountType of the User for displaying.
+     * @return the User's AccountType
      */
     public AccountType getAccountType() {
         return accountType;
     }
 
     /**
-     *
-     * @return
+     * Get the OrganisationalUnit of the User for displaying.
+     * @return the User's OrganisationalUnit
      */
     public OrganisationalUnit getOrganisationalUnit() {
         return organisationalUnit;
     }
 
     /**
-     *
-     * @return
+     * Get the username of the User for displaying.
+     * @return the User's username
      */
     public String getUsername() {
         return username;
     }
 
     /**
-     *
-     * @return
+     * Get the User ID of the User for displaying
+     * @return the User's ID
      */
     public String getUserId() {
         return userId;

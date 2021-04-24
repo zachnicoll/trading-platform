41169d4a59aa0461a55a3b9fe9b96973365ba35e
Rory Dennis <snoordennis@gmail.com>
2021-04-24 11:33:08 +1000

    chore:merged migration sql scripts and renamed folder

--- Migrations/V1_Setup_Enums.sql
+++
@@ -1,7 +0,0 @@
-/*
-Creates enumerated types for accounts, trades, and trade status.
-*/
-
-CREATE TYPE accountType AS ENUM ('USER', 'ADMIN');
-CREATE TYPE tradeType AS ENUM('BUY', 'SELL');
-CREATE TYPE statusType AS ENUM('RESOLVED','UNRESOLVED');

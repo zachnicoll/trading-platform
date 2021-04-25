382a4b26673533c308f20dbe0b30181331481278
Rory Dennis <snoordennis@gmail.com>
2021-04-25 16:01:29 +1000

    chore: updated tradeId in Trade from string to UUID

--- shared/src/models/Trade.java
+++ shared/src/models/Trade.java
@@ -1,11 +1,18 @@
 package models;
 
 import java.util.Date;
+import java.util.UUID;
 
 /**
  * Class for storing and retrieving a Trade's information.
  */
 public class Trade {
+
+    /**
+     * Unique UUID identifier for each trade
+     */
+    private final UUID tradeId;
+
     /**
      * Whether the Trade is a BUY or SELL order
      */
@@ -38,6 +45,7 @@ public class Trade {
 
     /**
      * Construct a new User with given information, most likely provided by the API.
+     * @param tradeId Unique UUID identifier for each trade
      * @param tradeType whether the trade is a BUY or SELL order
      * @param organisationalUnit the OrganisationalUnit that placed the Trade order
      * @param assetType the AssetType that is being bought/sold
@@ -45,7 +53,8 @@ public class Trade {
      * @param pricePerAsset price for a single asset of type AssetType
      * @param date date that the trade order was opened
      */
-    public Trade (TradeType tradeType, OrganisationalUnit organisationalUnit, AssetType assetType, Integer quantity, Float pricePerAsset, Date date) {
+    public Trade (UUID tradeId, TradeType tradeType, OrganisationalUnit organisationalUnit, AssetType assetType, Integer quantity, Float pricePerAsset, Date date) {
+        this.tradeId = tradeId;
         this.tradeType = tradeType;
         this.organisationalUnit = organisationalUnit;
         this.assetType = assetType;
@@ -53,6 +62,16 @@ public class Trade {
         this.pricePerAsset = pricePerAsset;
         this.date = date;
     }
+
+
+    /**
+     * Get the UUID unique identifier of the Trade
+     * @return the Trade's tradeId
+     */
+    public UUID getTradeId() {
+        return tradeId;
+    }
+
 
     /**
      * Get the AssetType of the Trade
@@ -68,6 +87,13 @@ public class Trade {
      */
     public TradeType getTradeType() {
         return tradeType;
+    }
+
+    /**
+     * Sets the trade type (buy or sell) of the Trade
+     */
+    public void setTradeType(TradeType tradeType) {
+        this.tradeType = tradeType;
     }
 
     /**

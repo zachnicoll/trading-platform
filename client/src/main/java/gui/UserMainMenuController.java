441cab43e480be874c900139afac0d83813221d5
N10223436 <n10223436@qut.edu.au>
2021-04-29 00:31:19 +1000

    Add extra user pages and their respective controller classes

--- client/src/main/java/gui/UserMainMenuController.java
+++ client/src/main/java/gui/UserMainMenuController.java
@@ -1,4 +1,57 @@
 package gui;
 
-public class UserMainMenuController {
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UserMainMenuController implements Initializable {
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private AnchorPane mainPane;
+
+    @FXML
+    private JFXButton btnDashboard;
+
+    @FXML
+    private JFXButton btnMarketplace;
+
+    @FXML
+    private JFXButton btnTradeHistory;
+
+    @FXML
+    private JFXButton btnProfile;
+
+    @FXML
+    private JFXButton btnLogout;
+
+    public void sceneDashboard(javafx.event.ActionEvent event) throws IOException {
+        System.out.println("btn pressed");
+        fxmlLoader object = new fxmlLoader();
+        Pane view = object.getPage("UserDashboard");
+        mainPane.getChildren().setAll(view);
+    }
+
+    public void sceneMarketplace(javafx.event.ActionEvent event) throws IOException {
+        System.out.println("btn pressed");
+        fxmlLoader object = new fxmlLoader();
+        Pane view = object.getPage("UserMarketplace");
+        mainPane.getChildren().setAll(view);
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //TODO
+    }
+
 }

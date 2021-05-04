d2c0adc0ed10cb19af26e279e8f30952831e6bd2
Zach Nicoll <zach.nicoll55@gmail.com>
2021-05-04 11:46:22 +1000

    fix: [#12] foreign key contraints in migrations scripts, added dummy data script

---
+++ migrations/v1/v1_downgrade.sql
@@ -0,0 +1,15 @@
+DROP table if exists public."assetTypes" CASCADE;
+
+DROP table if exists public."organisationalUnitAssets" CASCADE;
+
+DROP table if exists public."organisationalUnits" CASCADE;
+
+DROP table if exists public.trades CASCADE;
+
+DROP table if exists public.users CASCADE;
+
+DROP type if exists accounttype;
+
+DROP type if exists statustype;
+
+DROP type if exists tradetype;

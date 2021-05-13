242b27599ea0634e18d48ba050fe4cebdcc55ff8
N10223436 <n10223436@qut.edu.au>
2021-05-13 16:06:10 +1000

    working add button and saving new ou name and balance

--- client/src/main/java/gui/AdminOUMgmtController.java
+++ client/src/main/java/gui/AdminOUMgmtController.java
@@ -1,4 +1,33 @@
 package gui;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
 public class AdminOUMgmtController {
+
+    @FXML
+    private TextField txtNewOUName;
+
+    @FXML
+    private TextField txtNewOUBalance;
+
+    @FXML
+    private JFXButton btnAddOU;
+
+    @FXML
+    void addOU(ActionEvent event) {
+        //TODO implement add OU
+        String newOU;
+        Float newOUBalance;
+
+        newOU = txtNewOUName.getText();
+        newOUBalance = Float.parseFloat(txtNewOUBalance.getText());
+
+        System.out.println("New OU Name:" + newOU + "\tNew OU Balance: $" + newOUBalance);
+    }
+
 }

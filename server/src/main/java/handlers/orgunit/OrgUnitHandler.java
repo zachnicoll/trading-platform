339e621333e4eea813cc6915a0a73b871a3c3789
Rory Dennis <snoordennis@gmail.com>
2021-05-18 21:49:21 +1000

    feat:[#24] updated handlepost for creating new org units

--- server/src/main/java/handlers/orgunit/OrgUnitHandler.java
+++ server/src/main/java/handlers/orgunit/OrgUnitHandler.java
@@ -1,12 +1,15 @@
 package handlers.orgunit;
 
 import com.sun.net.httpserver.HttpExchange;
+import database.datasources.AssetDataSource;
 import database.datasources.OpenTradeDataSource;
 import database.datasources.OrganisationalUnitDataSource;
+import errors.JsonError;
 import handlers.AbstractRequestHandler;
 import models.Asset;
 import models.OpenTrade;
 import models.OrganisationalUnit;
+import models.partial.PartialOrganisationalUnit;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -40,14 +43,34 @@ public class OrgUnitHandler extends AbstractRequestHandler {
         checkIsAdmin(exchange);
 
         // Make new OrganisationalUnit object from json in request body
-        OrganisationalUnit newOrganisationalUnit = (OrganisationalUnit) readRequestBody(exchange, OrganisationalUnit.class);
+        PartialOrganisationalUnit partialOrganisationalUnit = (PartialOrganisationalUnit) readRequestBody(exchange, OrganisationalUnit.class);
+        OrganisationalUnit fullOrganisationalUnit =
+                new OrganisationalUnit(
+                        UUID.randomUUID(),
+                        partialOrganisationalUnit.unitName,
+                        partialOrganisationalUnit.creditBalance,
+                        new ArrayList<Asset>()
+                );
+
+        //check if unit name is not null
+        if(fullOrganisationalUnit.getUnitName() == null){
+            writeResponseBody(exchange, new JsonError("Organisational Unit does not have name"));
+            return;
+        }
+
+        //check if unit's credit balance is less than zero
+        if(fullOrganisationalUnit.getCreditBalance() <= 0){
+            writeResponseBody(exchange, new JsonError("Organisational Unit has credit balance less than or equal to zero"));
+            return;
+        }
+
 
         // Create new OrganisationalUnit in DB
         OrganisationalUnitDataSource organisationalUnitDataSource = new OrganisationalUnitDataSource();
-        organisationalUnitDataSource.createNew(newOrganisationalUnit);
+        organisationalUnitDataSource.createNew(fullOrganisationalUnit);
 
         // Respond with created Object
-        writeResponseBody(exchange, newOrganisationalUnit);
+        writeResponseBody(exchange, fullOrganisationalUnit);
     }
 }
 
---
+++ shared/src/main/java/models/partial/PartialOrganisationalUnit.java
@@ -0,0 +1,21 @@
+package models.partial;
+
+
+/**
+ * Class similar to the OrganisationalUnit class, but contains no methods and all attributes
+ * are publicly accessible. This class is used when creating a new OrganisationalUnit,
+ * as only certain attributes need to be sent to the Rest API. For example,
+ * the OrganisationalUnitId is generated on the Server and therefore should not be sent
+ * with a POST request to the /orgunit/ endpoint.
+ */
+public class PartialOrganisationalUnit {
+    public final String unitName;
+    public final Float creditBalance;
+
+
+    public PartialOrganisationalUnit (String unitName, Float creditBalance) {
+        this.unitName = unitName;
+        this.creditBalance = creditBalance;
+    }
+}
+

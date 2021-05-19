056b113dbb1824f7c7f37a20e94b4c44d9dfb8ac
Zach Nicoll <zach.nicoll55@gmail.com>
2021-05-19 22:07:32 +1000

    feat: [#13] implement backend for fetching all assets by org unit id

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,21 +1,9 @@
-import database.datasources.UserDataSource;
 import server.RestApi;
 import server.TradeResolver;
 
 import java.io.IOException;
 import java.util.Timer;
-import java.util.UUID;
-import database.datasources.ResolvedTradeDataSource;
-import database.DBConnection;
-import models.ResolvedTrade;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.UUID;
-import models.User;
 
 
 public class Main {
@@ -30,15 +18,5 @@ public class Main {
         Timer time = new Timer();
         TradeResolver tradeResolver = new TradeResolver();
         time.schedule(tradeResolver, 0, tradeResolvePeriod);
-
-        /*UserDataSource test = new UserDataSource();
-        ArrayList<User> temp = test.getAll();
-        for (User user : temp) {
-            System.out.println(user.getUsername());
-        }
-        /*ResolvedTrade thisone = test.getById(UUID.fromString("9b406cb0-eddb-4e5a-a6b8-e80fc088284f"), UUID.fromString("467c404d-a5ad-4359-9170-523f862ef028"));
-
-        System.out.println("DONE...");
-        System.out.println(thisone.getDateResolved() +"" + thisone.getQuantity());*/
     }
 }
--- server/src/main/java/handlers/assets/AssetsHandler.java
+++ server/src/main/java/handlers/assets/AssetsHandler.java
@@ -1,17 +1,21 @@
 package handlers.assets;
 
 import com.sun.net.httpserver.HttpExchange;
+import database.datasources.AssetDataSource;
 import handlers.AbstractRequestHandler;
 import models.Asset;
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Route: /assets/
- *
+ * <p>
  * Supported Methods:
- *
  */
 public class AssetsHandler extends AbstractRequestHandler {
 
@@ -20,8 +24,20 @@ public class AssetsHandler extends AbstractRequestHandler {
     }
 
     @Override
-    protected void handleGet(HttpExchange exchange) throws IOException {
-        ArrayList<Asset> assets = new ArrayList<>();
+    protected void handleGet(HttpExchange exchange) throws IOException, SQLException {
+        AssetDataSource assetDataSource = new AssetDataSource();
+        String[] params = exchange.getRequestURI().getRawPath().split("/");
+        ArrayList<Asset>  assets;
+
+        if (params.length == 3) {
+            // Organisational Unit Id is present in URL, use it to filter assets
+            UUID orgUnitId = UUID.fromString(params[2]);
+            assets = assetDataSource.getByOrgUnitId(orgUnitId);
+        } else {
+            // Otherwise just get all
+            assets = assetDataSource.getAll();
+        }
+
         writeResponseBody(exchange, assets);
     }
 }

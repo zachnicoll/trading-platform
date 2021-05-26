b8419b7ccdb5429f43c54550db3a22f3949a42b1
Rory Dennis <snoordennis@gmail.com>
2021-05-26 16:56:14 +1000

    feat:[#37] setup client dashboard controller and fxml

--- client/src/main/java/gui/UserDashboardController.java
+++ client/src/main/java/gui/UserDashboardController.java
@@ -1,4 +1,32 @@
 package gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class UserDashboardController {
+
+    @FXML
+    private TableView<?> openTradesTable;
+
+    @FXML
+    private TableColumn<?, ?> tblcolAsset;
+
+    @FXML
+    private TableColumn<?, ?> tblcolPrice;
+
+    @FXML
+    private TableColumn<?, ?> tblcolQuantity;
+
+    @FXML
+    private TableColumn<?, ?> tblcolOr;
+
+    @FXML
+    private TextField txtFieldSearch;
+
+    @FXML
+    private Text txtUnitBalance;
+
 }

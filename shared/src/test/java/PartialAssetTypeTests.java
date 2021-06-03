e7841159f42814a615d25aa659a485e120408d3c
N10223436 <n10223436@qut.edu.au>
2021-06-03 11:29:50 +1000

    tests: PartialAssetTypeTests

---
+++ shared/src/test/java/PartialAssetTypeTests.java
@@ -0,0 +1,30 @@
+import models.partial.PartialAssetType;
+import org.junit.jupiter.api.*;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PartialAssetTypeTests {
+
+    /*
+     * Test 0: Declaring PartialAssetType object
+     */
+    String assetName = "test asset";
+    PartialAssetType partialAssetType;
+
+    /* Test 1: Constructing an Asset object
+     */
+    @BeforeEach
+    @Test
+    public void setUpPartialAssetType() {
+        partialAssetType = new PartialAssetType(assetName);
+    }
+
+    /* Test 2: Get PartialAssetTypes's Name
+     */
+    @Test
+    public void getPartialAssetTypeName() {
+        assertEquals(partialAssetType.assetName, "test asset");
+    }
+
+}

5627627fe1e56fac49bf6390227c576c149e7175
Zach Nicoll <zach.nicoll55@gmail.com>
2021-06-05 15:17:42 +1000

    test: [#56] login handler tests complete, 90 percent coverage

--- server/src/test/java/LoginHandlerTests.java
+++ server/src/test/java/LoginHandlerTests.java
@@ -1,7 +1,6 @@
 import com.google.gson.Gson;
 import data.LoginHandlerDataGenerator;
-import data.TradesHandlerDataGenerator;
-import helpers.PasswordHasher;
+import errors.JsonError;
 import models.AuthenticationToken;
 import models.Credentials;
 import org.junit.jupiter.api.AfterEach;
@@ -23,8 +22,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LoginHandlerTests {
     private final HttpClient client = HttpClient.newHttpClient();
     private final Gson gson = new Gson();
-    private HttpRequest.Builder httpBuilder;
     LoginHandlerDataGenerator loginHandlerDataGenerator;
+    private HttpRequest.Builder httpBuilder;
 
     @BeforeAll
     @Test
@@ -45,8 +44,11 @@ public class LoginHandlerTests {
                 .header("Content-Type", "application/json");
     }
 
+    /**
+     * Test 1: Login with correct username and password
+     */
     @Test
-    public void loginWithCorrectCredentials() throws SQLException, IOException, InterruptedException {
+    public void loginWithCorrectCredentials() throws IOException, InterruptedException {
         Credentials credentials = new Credentials(loginHandlerDataGenerator.username, loginHandlerDataGenerator.password);
 
         HttpRequest request = httpBuilder.POST(HttpRequest.BodyPublishers.ofString(gson.toJson(credentials))).build();
@@ -57,6 +59,57 @@ public class LoginHandlerTests {
         // This should NOT throw an error
         gson.fromJson(response.body(), AuthenticationToken.class);
     }
+
+    /**
+     * Test 2: Login with incorrect username and password
+     */
+    @Test
+    public void loginWithInCorrectCredentials() throws IOException, InterruptedException {
+        Credentials credentials = new Credentials(loginHandlerDataGenerator.username, "incorrect password");
+
+        HttpRequest request = httpBuilder.POST(HttpRequest.BodyPublishers.ofString(gson.toJson(credentials))).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        assertEquals(response.statusCode(), 403);
+
+        // This should NOT throw an error
+        JsonError jsonError = gson.fromJson(response.body(), JsonError.class);
+        assertEquals(jsonError.getError(), "Username or password incorrect");
+    }
+
+    /**
+     * Test 3: Send request to login endpoint with request body
+     */
+    @Test
+    public void loginNoBody() throws IOException, InterruptedException {
+        HttpRequest request = httpBuilder.POST(HttpRequest.BodyPublishers.ofString("")).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        assertEquals(response.statusCode(), 403);
+
+        // This should NOT throw an error
+        JsonError jsonError = gson.fromJson(response.body(), JsonError.class);
+        assertEquals(jsonError.getError(), "Username or password incorrect");
+    }
+
+    /**
+     * Test 4: Login with username that does not exist
+     */
+    @Test
+    public void loginUsernameDoesNotExist() throws IOException, InterruptedException {
+        Credentials credentials = new Credentials("non-existent user", loginHandlerDataGenerator.password);
+
+
+        HttpRequest request = httpBuilder.POST(HttpRequest.BodyPublishers.ofString(gson.toJson(credentials))).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        assertEquals(response.statusCode(), 403);
+
+        // This should NOT throw an error
+        JsonError jsonError = gson.fromJson(response.body(), JsonError.class);
+        assertEquals(jsonError.getError(), "Username or password incorrect");
+    }
+
 
     @AfterEach
     @Test

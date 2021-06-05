548e0e444d50908733ab41de6bda438d3d4ad27c
Zach Nicoll <zach.nicoll55@gmail.com>
2021-06-05 23:18:46 +1000

    feat: [#55] adding gradle task for running client and server

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,10 +1,4 @@
 import gui.GraphicalUserInterface;
-import helpers.AuthRefresh;
-import helpers.ClientInfo;
-
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.Timer;
 
 public class Main {
     public static void main(String[] args) {
